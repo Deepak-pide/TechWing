906dc4e95301f9fdae8af56e3302d7b307684ba6
Deepak Gupta <dg3667610@gmail.com>
2025-09-09 08:52:51 +0000

    show header fixed make in global that show in all pages

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -10,10 +10,11 @@ export default function AppLayout({ children }: { children: ReactNode }) {
 
   return (
     <div className="flex min-h-screen flex-col">
-      {isMobile ? <FooterNav /> : <Header />}
+      <Header />
       <main className={`flex-1 ${isMobile ? 'pb-20 pt-4' : 'py-8'}`}>
         {children}
       </main>
+      {isMobile && <FooterNav />}
     </div>
   );
 }
