61e87c01df3e6e501d184eb5f6a302c5806b8418
Deepak Gupta <dg3667610@gmail.com>
2025-09-27 02:16:33 +0000

    make not have account signup in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,13 @@
 import LoginForm from "./LoginForm";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <div className="relative flex flex-1 w-full items-center justify-center p-4">
+    <div className="relative flex flex-1 w-full flex-col items-center justify-center p-4">
        <Image
-          src="/Farmland.jpg"
+          src="/farmland.jpg"
           alt="Farmland background"
           fill
           className="object-cover -z-10 brightness-50 blur-sm"
@@ -23,6 +24,12 @@ export default function LoginPage() {
           <LoginForm />
         </CardContent>
       </Card>
+      <p className="text-center text-sm text-white mt-6">
+        Don't have an account?{" "}
+        <Link href="#" className="font-semibold underline">
+          Sign up
+        </Link>
+      </p>
     </div>
   );
 }
